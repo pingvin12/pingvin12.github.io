de77c9ea3f4dc9b9c38f1631e0c72d764da83037
pingvin12 <pingvin12@users.noreply.github.com>
2022-07-26 22:37:36 +0000

    deploy: 1ffae91284cce36b295855cfc2de08e5f1d3a4a8

---
+++ _next/static/Y_uQLaPgNUENUedK5GwK8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ee9594b91b258940.js","static/chunks/163-2e6981d8c3f408ee.js","static/chunks/pages/index-afc45187e654ae66.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
