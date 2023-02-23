25d4090438e1ac9a9c368e47aec9f984c525c332
pingvin12 <pingvin12@users.noreply.github.com>
2023-02-23 01:52:16 +0000

    deploy: 3f3de67c73da67fff5fcfc9a4c19b283c2a90365

--- _next/static/chunks/pages/home-c946689a7b1d6b09.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{4309:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(6934)}])}},function(n){n.O(0,[445,694,934,774,888,179],(function(){return _=4309,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
