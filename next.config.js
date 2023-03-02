9c3c3bf121bfdd15e3507f563feb206e6824c9f7
milliorn <scottmilliorn@gmail.com>
2023-03-01 22:02:52 -0800

    element does not have a [lang] attribute

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// If your website supports multiple languages, you can put various values in the locales array.
+
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
