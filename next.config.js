2c22c1698e8b77bead2cb7c18c51210532d52c43
pingvin12 <fenyesj@gmail.com>
2023-07-03 09:25:51 +0200

    chore(ci): change ci

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 // If your website supports multiple languages, you can put various values in the locales array.
 
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  output: 'export'
 };
