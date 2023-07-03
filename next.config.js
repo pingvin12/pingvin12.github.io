1369b7afa105989b78b360206686105b94d25b4d
pingvin12 <fenyesj@gmail.com>
2023-07-03 12:50:59 +0200

    feat(analytics): add google analytics

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 // If your website supports multiple languages, you can put various values in the locales array.
 
 module.exports = {
-  output: 'export'
+  output: 'export',
+  env: {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: 'G-YDV2R1TLWM',
+  },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,22 +35,20 @@ function MyApp({Component, pageProps}: AppProps) {
                 />
                 <meta property="twitter:image" content="/avatar.png"/>
             </Head>
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+<Script strategy="lazyOnload">
+    {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+</Script>
 
             <Component {...pageProps} />
-
-            <Script
-                strategy="lazyOnload"
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-            ></Script>
-            <Script id="google-analytics" strategy="lazyOnload">
-                {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-        `}
-            </Script>
         </>
     );
 }
